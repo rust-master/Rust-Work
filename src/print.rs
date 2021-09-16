d79341fe6ac74cd80329f9dd96cd95da5b516a55
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-09-16 21:33:45 +0500

    Positional Arguments

--- src/print.rs
+++ src/print.rs
@@ -1,6 +1,9 @@
 pub fn run(){
     println!("Hello from the main.rs file");
 
-
+    // Basic Formatting
     println!("{} is Expert {}", "Zaryab", "Blockchain Developer");
+
+    // Positional Arguments
+    println!("{0} is Expert {1} and {0} is Expert {2} {3}", "Zaryab", "Blockchain Developer", "with", "Rust"); 
 }
