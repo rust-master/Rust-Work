b11a273621dcf256f4454e9a7bc44eea763f6425
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-09-16 21:36:35 +0500

    formatting

--- src/print.rs
+++ src/print.rs
@@ -1,9 +1,12 @@
-pub fn run(){
+pub fn run() {
     println!("Hello from the main.rs file");
 
     // Basic Formatting
     println!("{} is Expert {}", "Zaryab", "Blockchain Developer");
 
     // Positional Arguments
-    println!("{0} is Expert {1} and {0} is Expert {2} {3}", "Zaryab", "Blockchain Developer", "with", "Rust"); 
+    println!(
+        "{0} is Expert {1} and {0} is Expert {2} {3}",
+        "Zaryab", "Blockchain Developer", "with", "Rust"
+    );
 }
