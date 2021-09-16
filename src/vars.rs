533b109e9add6a0a0e332d03926930e4f59f8806
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-09-16 22:38:09 +0500

    formatting

--- src/vars.rs
+++ src/vars.rs
@@ -1,4 +1,3 @@
-
 pub fn run() {
     let name = "Zaryab";
     let mut age = 22;
@@ -7,12 +6,10 @@ pub fn run() {
     println!("My Name is , {}! and I am {}", name, age);
 
     // Define const
-    const ID:  i32 = 103;
+    const ID: i32 = 103;
     println!("ID is {}", ID);
 
     // Assign multiple variables
     let (name, age) = ("Zaryab", 22);
     println!("My Name is , {}! and I am {}", name, age);
-
-    
 }
