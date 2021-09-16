2243bfb459498baf8d82416e07a10d29c1399b10
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-09-16 22:37:58 +0500

    variables in rust

---
+++ src/vars.rs
@@ -0,0 +1,18 @@
+
+pub fn run() {
+    let name = "Zaryab";
+    let mut age = 22;
+    println!("My Name is , {}! and I am {}", name, age);
+    age = 23;
+    println!("My Name is , {}! and I am {}", name, age);
+
+    // Define const
+    const ID:  i32 = 103;
+    println!("ID is {}", ID);
+
+    // Assign multiple variables
+    let (name, age) = ("Zaryab", 22);
+    println!("My Name is , {}! and I am {}", name, age);
+
+    
+}
