5e15b15a446f1292cab70948a328e180606035a1
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-09-24 16:53:37 +0500

    conditions in rusts

--- src/conditional.rs
+++ src/conditional.rs
@@ -1,11 +1,23 @@
 pub fn run() {
-    let age = 19;
+    let age = 18;
+    let check_id = false;
 
     // If-else
-    if age >= 18 {
+    if age >= 18 && check_id {
         println!("You can vote!");
-    } else {
+    } else if age < 18 && check_id {
         println!("You can't vote!");
+    } else {
+        println!("Give you ID");
     }
-    
+
+    // Shorthand if-else
+    let vote_age = if age >= 18 && check_id {
+        "You can vote!"
+    } else {
+        "You can't vote!"
+    };
+
+    println!("{}", vote_age);
+
 }
