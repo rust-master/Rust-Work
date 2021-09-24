86f6ef7f7979f679565894d2fadb3d762b83ae74
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-09-24 17:27:33 +0500

    Tuple Struct

--- src/structs.rs
+++ src/structs.rs
@@ -1,4 +1,3 @@
-
 // Traditional Struct
 struct Color {
     red: u8,
@@ -6,12 +5,22 @@ struct Color {
     blue: u8,
 }
 
-pub fn run() {
-    let mut c = Color {
-        red: 255,
-        green: 0,
-        blue: 0,
-    };
+// Tuple Struct
+struct ColorTuple(u8, u8, u8);
 
-    println!("Color: {} {} {}", c.red, c.green, c.blue);
+pub fn run() {
+    // let mut c = Color {
+    //     red: 255,
+    //     green: 0,
+    //     blue: 0,
+    // };
+
+    // c.red = 200;
+
+    // println!("Color: {} {} {}", c.red, c.green, c.blue);
+
+    let mut ct = ColorTuple(255, 0, 0);
+
+    ct.0 = 200;
+    println!("Color: {} {} {}", ct.0, ct.1, ct.2);
 }
