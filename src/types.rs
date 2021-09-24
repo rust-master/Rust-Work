88208018b1c36db5777f3cf302d963532f6a3ae3
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-09-24 14:53:13 +0500

    max size

---
+++ src/types.rs
@@ -0,0 +1,13 @@
+pub fn run() {
+
+    let x= 5;
+    
+    let y = 2.5;
+
+    let z: i64 = 12331321312;
+
+    // fine max size
+    println!("Max i32: {}", std::i32::MAX);
+    println!("Max i64: {}", std::i64::MAX);
+
+}
