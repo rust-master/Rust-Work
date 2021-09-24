4b222f411156aba0f1e60191bd9e2f65f280b475
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-09-24 16:44:10 +0500

    vectors in rust

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@
 // mod types;
 // mod strings;
 // mod tuples;
-mod arrays;
+// mod arrays;
+mod vectors;
 
 fn main() {
     // print::run();
@@ -11,5 +12,6 @@ fn main() {
     // types::run();
     // strings::run();
     // tuples::run();
-    arrays::run();
+    // arrays::run();
+    vectors::run();
 }
