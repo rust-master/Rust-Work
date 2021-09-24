7d55524d921eca2ec3198f421c5f50255025d2a8
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-09-24 17:46:53 +0500

    structs in rust

--- src/structs.rs
+++ src/structs.rs
@@ -6,7 +6,38 @@ struct Color {
 }
 
 // Tuple Struct
-struct ColorTuple(u8, u8, u8);
+// struct ColorTuple(u8, u8, u8);
+
+struct Person {
+    first_name: String,
+    last_name: String,
+}
+
+impl Person {
+    // Constructor
+    fn new(first: &str, last: &str) -> Person {
+        Person {
+            first_name: first.to_string(),
+            last_name: last.to_string(),
+        }
+    }
+
+    // Get full name
+    fn full_name(&self) -> String {
+        format!("{} {}", self.first_name, self.last_name)
+    }
+
+    // Set last name
+    fn set_first_name(&mut self, first: &str) {
+        self.first_name = first.to_string();
+    }
+
+    // Name to tuple
+    fn to_tuple(self) -> (String, String) {
+        (self.first_name, self.last_name)
+    }
+
+}
 
 pub fn run() {
     // let mut c = Color {
@@ -19,8 +50,19 @@ pub fn run() {
 
     // println!("Color: {} {} {}", c.red, c.green, c.blue);
 
-    let mut ct = ColorTuple(255, 0, 0);
+    // let mut ct = ColorTuple(255, 0, 0);
 
-    ct.0 = 200;
-    println!("Color: {} {} {}", ct.0, ct.1, ct.2);
+    // ct.0 = 200;
+    // println!("Color: {} {} {}", ct.0, ct.1, ct.2);
+
+    let mut p = Person::new("Zaryab", "Rafique");
+
+    println!("Person: {}", p.full_name());
+
+    p.set_first_name("Muhammad Zaryab");
+
+    println!("Person: {}", p.full_name());
+
+    println!("Person Tuple: {:?}", p.to_tuple());
+    
 }
