2a157036e5e21bde00514db5429fe4655992164b
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-09-24 16:21:49 +0500

    strings in rust

--- src/strings.rs
+++ src/strings.rs
@@ -3,11 +3,42 @@ pub fn run() {
 
     println!("Name: {}", name);
 
+    // Push a character to the string
+    name.push(' ');
+
+    // Push a string to the string
+    name.push_str("Rafique");
+
+    // Capacity in bytes
+    println!("Capacity: {}", name.capacity());
+
     // Get the length of the string
     println!("Length: {}", name.len());
 
-    // Push a character to the string
-    name.push('R');
+    // Check if string is empty
+    println!("Is empty: {}", name.is_empty());
+
+    // Contains
+    println!("Contains 'Z': {}", name.contains("Z"));
+
+    // Replace
+    println!("Replace: {}", name.replace("Z", "A"));
+
+    // Loop through string by whitespace
+    for word in name.split_whitespace() {
+        println!("Word: {}", word);
+    }
+
+    // Create string with capacity
+    let mut s = String::with_capacity(10);
+    s.push('a');
+    s.push('b');
+
+    println!("{}", s);
+
+    // Assertion testing
+    assert_eq!(2, s.len());
+    assert_eq!(10, s.capacity());
 
     println!("Push: {}", name);
 }
